31fdb63464a58ab5c769cca803bce3d85d4fc59b
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-26 08:46:31 +0000

    edit function called removeColor

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ function setColor(element) {
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {}
+function removeColor(element) {
+  element.style.background = "#1d1d1d";
+  element.style.boxShadow = "0 0 2px #000";
+}
 
 function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
